d4c548ba91d60aabdd3db3a140cd7d4c4d15ea4c
jahanavisinha <sinha.j@northeastern.edu>
2024-12-08 00:32:21 -0500

    logging in works

--- spotify-react-web/src/vite-env.d.ts
+++ spotify-react-web/src/vite-env.d.ts
@@ -1,12 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-    readonly VITE_SPOTIFY_CLIENT_ID : string;
-    //77693689203b4c61b7c292e35603f65e
-    readonly VITE_SPOTIFY_CLIENT_SECRET : string;
-    //5318e4996d0b4e588cbae945996a62ad
-    readonly VITE_SCOPES : string;
-    // user-read-private user-read-email
-    readonly VITE_SPOTIFY_REDIRECT_URI : string;
-    // http://localhost:5173/home
-}
--- spotify-react-web/vite.config.ts
+++ spotify-react-web/vite.config.ts
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-  },
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 5173,
+//   },
+// })
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    base: env.VITE_ROUTER_BASE_URL || '/',
+    define: {
+      'process.env': env,
+    },
+    plugins: [react()],
+    server: {
+      port: 5173,
+    },
+  })
+}
